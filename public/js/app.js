70567632cee41aa2f8a9943d269b2b511b96ea19
Ryan Wade <ryanwade1928@gmail.com>
2019-07-10 17:10:52 -0500

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,4 @@
 /* eslint-disable no-console */
-console.log("Client Side JS File Loaded");
-
-fetch("http://puzzle.mead.io/puzzle").then(response => {
-    response.json().then(data => {
-        console.log(data);
-    });
-});
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
@@ -18,16 +10,15 @@ weatherForm.addEventListener("submit", event => {
     const location = search.value;
     messageOne.textContent = "Loading";
     messageTwo.textContent = "";
-    fetch("http://localhost:3000/weather?address=" + location).then(
-        response => {
-            response.json().then(data => {
-                if (data.error) {
-                    messageOne.textContent = data.error;
-                } else {
-                    messageOne.textContent = data.location;
-                    messageTwo.textContent = data.forecast;
-                }
-            });
-        }
-    );
+
+    fetch("/weather?address=" + location).then(response => {
+        response.json().then(data => {
+            if (data.error) {
+                messageOne.textContent = data.error;
+            } else {
+                messageOne.textContent = data.location;
+                messageTwo.textContent = data.forecast;
+            }
+        });
+    });
 });
